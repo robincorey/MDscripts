ff7e5cd2cf4299c2326b3d92d564940c7edbb4da
Robin Corey <robin.corey@gmail.com>
2016-11-06 13:45:23 +0000

    Updating MM setup script - notes in file

--- Atomistic/memprotscript.sh
+++ Atomistic/memprotscript.sh
@@ -4,13 +4,14 @@
 # Modified for SecA inclusion - specific for this system
 # 
 
+###### MODIFIED 24/11/2014 for new gmembed - including NUC in gmembed option###############
 
 #
 ## Need to define prot and nuc options (ATP/ADP)
 #
 
-prot=ATPpos
-nuc=TPp
+prot=AYEG
+nuc=ATP
 mem=POP
 
 # get equilibrated POPC membrane and other setup files
@@ -19,9 +20,9 @@ cp /home/robinc/Desktop/MD_KIT/512-popc-eq.pdb .
 cp /home/robinc/Desktop/MD_KIT/SOL.gro .   # Added to get around g_membed issues in 4.6.5
 cp /home/robinc/Desktop/MD_KIT/*mdp .
 cp /home/robinc/Desktop/MD_KIT/*gro .
-cp /home/robinc/Desktop/MD_KIT/membed.dat .
 cp /home/robinc/Desktop/MD_KIT/*itp .
 cp /home/robinc/Desktop/MD_KIT/popc_opls.itp .
+cp /home/robinc/Desktop/MD_KIT/membed.dat .
 
 #
 ## Generate topology file from pdb
@@ -32,122 +33,72 @@ editconf -f $prot.gro -o $prot.gro.pdb
 sed -e 's/TER/   /g' -e 's/ENDMDL/      /g' $prot.gro.pdb -i
 cat $prot.gro.pdb 512-popc-eq.pdb > protmem.gro.pdb
 
-grep -Ev 'REMARK|MODEL|TER|ENDMDL|TITLE|CRYST1' protmem.gro.pdb > protmem_out.gro.pdb
-grep -v "^$" protmem_out.gro.pdb > protmem_out_2.gro.pdb
+editconf -f protmem.gro.pdb -o $prot-mem.pdb.gro -c -box 13.1708 13.1708 15 -bt triclinic
+size=`tail -n 1 $prot-mem.pdb.gro`
+grep -v "^$" $prot-mem.pdb.gro > protmem.gro
+
+uniqatoms=`grep $mem $prot-mem.pdb.gro | awk '{print $1}' | uniq -c | tail -n 1 | awk '{print $1}'`
+totalatoms=`grep $mem $prot-mem.pdb.gro | awk '{print $1}' | wc -l`
+echo $mem `echo $totalatoms $uniqatoms | awk '{print $1/$2}'` >> $prot.top
+
+sed '/Include water topology/i \
+# include "'popc_opls.itp'"\
+' $prot.top -i 
 
 # 
 ## Topology created
 #
 
-editconf -f protmem_out_2.gro.pdb -o $prot-mem.pdb.gro -c -box 13.1708 13.1708 15 -bt triclinic
-#grep -e POP $prot-mem.pdb.gro | awk '{print $1}' |  sed 's/[0-9]//g' | uniq > species
-
-#for list in `cat species`
-#do
-#searchterm=`echo $list | sed 's/-/ /g' | awk '{print $1}'`
-#uniqatoms=`grep $searchterm $prot-mem.pdb.gro | awk '{print $1}' | uniq -c | tail -n 1 | awk '{print $1}'`
-#totalatoms=`grep $searchterm $prot-mem.pdb.gro | awk '{print $1}' | wc -l`
-#echo $searchterm  `echo $totalatoms $uniqatoms | awk '{print $1/$2}'` >> $prot.top
-#done
-
-uniqatoms=`grep $mem $prot-mem.pdb.gro | awk '{print $1}' | uniq -c | tail -n 1 | awk '{print $1}'`
-totalatoms=`grep $mem $prot-mem.pdb.gro | awk '{print $1}' | wc -l`
-echo $mem  `echo $totalatoms $uniqatoms | awk '{print $1/$2}'` >> $prot.top
-
-
-# adding itp file to top file
-
-sed '/Include water topology/i \
-#include "'popc_opls.itp'"\
-' $prot.top -i 
-
 #
-## Setting up gmembed - assumes gmembed.mdp is already completed. Use section from John's script if not
+## Setting up gmembed - MODIFIED FOR MUC INCLUSION
 #
 
-######## ADD SOL FOR GMEMBED
+# ADD SOL FOR GMEMBED
 
 genbox -cp $prot-mem.pdb.gro -o solmem.gro -p $prot.top -cs
-
-
-#sed '$d' < $prot-mem.pdb.gro > prot.gro
-#cat prot.gro SOL2.gro > solmem.gro
-#tail -n 1 $prot-mem.pdb.gro > bottom
-#cat bottom >> solmem.gro
-
-#sed -i -e "1,2d" solmem.gro
-#atomnumbercount1=`grep -c [0-9] solmem.gro | awk '{print $1 }'`
-#atomnumbercount2=`expr $atomnumbercount1 - 1`
-#sed '1i '$atomnumbercount2'' solmem.gro -i
-#sed '1i Membrane protein' solmem.gro -i
-
-#sed -i '/SOL/d' $prot.top
-
-#uniqatomssol=`grep SOL solmem.gro | awk '{print $1}' | uniq -c | tail -n 1 | awk '{print $1}'`
-#totalatomssol=`grep SOL solmem.gro | awk '{print $1}' | wc -l`
-#echo SOL `echo $totalatomssol $uniqatomssol | awk '{print $1/$2}'` >> $prot.top
-
-grompp -v -f gmembed.mdp -o gmembed -c solmem.gro -p $prot.top
-echo 1 POP | mdrun -s gmembed.tpr -membed membed.dat -o traj.trr -c membeddedsol.gro -e ener.edr -nt 1 -cpt -1 -v -stepout 100
-
-grep -Ev 'SOL' membeddedsol.gro > membedded.gro
-
-sed -i '/SOL/d' $prot.top
-
-#sed -i -e "1,2d" membedded.gro
-#cat $nuc.gro membedded.gro > $nuc-mem.gro
-#tail -n 1 $prot-mem.pdb.gro > bottom
-#atomnumbercount1=`grep -c [0-9] $nuc-mem.gro | awk '{print $1 }'`
-#atomnumbercount2=`expr $atomnumbercount1 - 1`
-#sed '1i '$atomnumbercount2''  $nuc-mem.gro -i
-#sed '1i Membrane protein' $nuc-mem.gro -i
-#cat bottom >> $nuc-mem.gro
-#######  EXTRA SOL REMOVED
-
-
-#
-## gmembed completed
-#
-
-
-
-#
-## Adding nucleotide to the protein - specify at beginning of script
-#
-
-sed -i -e "1,2d" membedded.gro
-cat $nuc.gro membedded.gro > $nuc-mem.gro
-tail -n 1 $prot-mem.pdb.gro > bottom
-atomnumbercount1=`grep -c [0-9] $nuc-mem.gro | awk '{print $1 }'`
-atomnumbercount2=`expr $atomnumbercount1 - 1`
-sed '1i '$atomnumbercount2''  $nuc-mem.gro -i
-sed '1i Membrane protein' $nuc-mem.gro -i
-cat bottom >> $nuc-mem.gro
+sed -i -e "1,2d" solmem.gro
+cat $nuc.gro solmem.gro > nucsolmem.gro
 
 sed -i '/; Compound        #mols/a \Other_chain_B 1\' $prot.top
 sed -e '/; Include chain topologies/a \#include "'"$nuc.itp"'"' $prot.top > tmptp
 cat tmptp > $prot.top
 
-sed -i '/POP/d' $prot.top
 
-uniqatoms=`grep $mem $nuc-mem.gro | awk '{print $1}' | uniq -c | tail -n 1 | awk '{print $1}'`
-totalatoms=`grep $mem $nuc-mem.gro | awk '{print $1}' | wc -l`
-echo $mem  `echo $totalatoms $uniqatoms | awk '{print $1/$2}'` >> $prot.top
+#tail -n 1 $prot-mem.pdb.gro > bottom
+#cat bottom >> solmem.gro
 
+atomnumbercount1=`grep -c [0-9] nucsolmem.gro | awk '{print $1 }'`
+atomnumbercount2=`expr $atomnumbercount1 - 1`
+sed '1i '$atomnumbercount2'' nucsolmem.gro -i
+sed '1i Membrane protein with Nucleotide' nucsolmem.gro -i
 
-#for list in `cat species`
-#do 
-#searchterm=`echo $list | sed 's/-/ /g' | awk '{print $1}'`
-#uniqatoms=`grep $searchterm $nuc-mem.gro | awk '{print $1}' | uniq -c | tail -n 1 | awk '{print $1}'`
-#totalatoms=`grep $searchterm $nuc-mem.gro | awk '{print $1}' | wc -l`
-#echo $searchterm `echo $totalatoms $uniqatoms | awk '{print $1/$2}'` >> $prot.top 
-#done
+sed -i '/SOL/d' $prot.top
+uniqatomssol=`grep SOL nucsolmem.gro | awk '{print $1}' | uniq -c | tail -n 1 | awk '{print $1}'`
+totalatomssol=`grep SOL nucsolmem.gro | awk '{print $1}' | wc -l`
+echo SOL `echo $totalatomssol $uniqatomssol | awk '{print $1/$2}'` >> $prot.top
+
+cp /home/robinc/Desktop/MD_KIT/*ndx .
+grompp -v -f gmembed_NUC.mdp -o gmembed -c nucsolmem.gro -p $prot.top -n gmem_$nuc.ndx
+echo NUC $mem | mdrun -s gmembed.tpr -membed membed.dat -o traj.trr -c membeddedsol.gro -e ener.edr -nt 1 -cpt -1 -v -stepout 100 -mn gmem_$nuc
+##############################
+grep -Ev 'SOL' membeddedsol.pdb > membedded.gro
+sed -i '/SOL/d' $prot.top
+
+sed -i -e "1,2d" membedded.gro
+atomnumbercount1=`grep -c [0-9] membedded.gro | awk '{print $1 }'`
+atomnumbercount2=`expr $atomnumbercount1 - 1`
+sed '1i '$atomnumbercount2''  membedded.gro -i
+sed '1i Membrane protein' membedded.gro -i
+
+#
+## gmembed completed
+#
 
 #
 ## Minimizing and solvating system
 #
 
-grompp -v -f em.mdp -o em1 -e em1 -c $nuc-mem.gro  -p $prot.top
+grompp -v -f em.mdp -o em1 -e em1 -c membedded.gro  -p $prot.top
 mdrun -v -s em1 -o em1 -e e1 -c after_em1.gro 
 editconf -f em1.tpr -o ref1.pdb
 
@@ -200,5 +151,6 @@ cat bottom2 >> all.gro
 
 grompp -v -f em -o em3 -c all.gro -p $prot.top
 mdrun -v -s em3 -o em3 -e em3 -c after_em3
+
 
 
