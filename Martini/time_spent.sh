1daa342e738461bc0e06a5cb28b6e13d7bad17ff
Robin Corey <robin.corey@gmail.com>
2016-12-15 18:02:36 +0000

    Doing some cleaning

--- Martini/time_spent.sh
+++ Martini/time_spent.sh
@@ -3,7 +3,6 @@
 # Can be used for every sim
 
 ARRAY=(192.small 192_reseed.1.small 192_reseed.2.small 192_reseed.3.small 192_reseed.4.small unbiased_BC3)
-#ARRAY=(unbiased_BC3)
 ARRAYLEN=`echo ${#ARRAY[@]} - 1 | bc`
 CURRENTDIR=`pwd`
 TPR=unbiased.tpr
@@ -25,7 +24,6 @@ chain3=`grep " 1GLU     BB" $GRO | awk '{print $3}'`; chain2b=`echo $chain3 - 1 
 chain4=`grep " 1HIS     BB" $GRO | awk '{print $3}'`; chain3b=`echo $chain4 - 1 | bc`
 chain4b=`grep " 817DPPC   NC3" $GRO | awk '{print $3}'`
 chain4c=`echo "$chain4b - 1" | bc`
-#echo -e a1-$chain0b'\n'a$chain1-$chain1b'\n'a$chain2-$chain2b'\n'a$chain3-$chain3b'\n'a$chain4-$chain4c'\n'q | make_ndx -f $GRO -o chains.ndx -n index.ndx
 indices=`grep -c "\[" chains.ndx`
 ## Groups for each chain
 PO4=`echo "$indices - 5" | bc`
@@ -33,10 +31,6 @@ A=`echo "$indices - 4" | bc`
 B=`echo "$indices - 3" | bc`
 C=`echo "$indices - 2" | bc`
 D=`echo "$indices - 1" | bc`
-#echo $A | editconf -f $GRO -o A.pdb -n chains.ndx
-#echo $B |editconf -f $GRO -o Y.pdb -n chains.ndx
-#echo $C |editconf -f $GRO -o E.pdb -n chains.ndx
-#echo $D |editconf -f $GRO -o G.pdb -n chains.ndx
 
 ARRAYA=(`grep "BB  ${RES}" A.pdb | awk '{print $5}'`)  # GLN as reasonably few, similar size
 ARRAYY=(`grep "BB  ${RES}" Y.pdb | awk '{print $5}'`)  # to lys, polar, and likely to be
@@ -65,7 +59,6 @@ for i in `seq 0 ${ARRAYALEN}`; do
 		##############"
 		exit 0
 	fi
-#	echo -e $indices'\n'PO1_PO2_GL0 | g_mindist -f ${ARRAY[j]}.pbc.xtc -s $TPR -n tempA -od SecA_${ARRAYA[i]}_${ARRAY[j]}_${RES}.xvg -nice -5 -tu us
 	echo SecA_${ARRAYA[i]} >> linger_${ARRAY[j]}_${RES}.xvg 
 	while IFS= read -r line
         do
