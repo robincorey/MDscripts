6afb2fc508d64c26646e9e3a091792ff8e7571f2
robincorey <robin.corey@gmail.com>
2018-02-01 10:29:37 +0000

    Changes - improving main set up script [skip ci]

--- Atomistic/memprot_post_pdb2gmx.sh
+++ Atomistic/memprot_post_pdb2gmx.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 # Script to run a membrane protein from pdb state to membrane embedded state
-# Need to make sure pdb file is numbered correctly and TER is used to break chains
-# Modified for SecA inclusion - specific for this system but should work for any prot
-# If no nucleotide might cause issues - need to check
-
 #
-## Need to define prot and nuc options (ATP/ADP)
-## Check variables are correctly assigned!!
-#
+# Modified for Oxford
 
 ############################
-prot=5aww_MTS		#### >>> Needs to match input PDB
-nuc=			#### >>> ADP or ATP (if none leave blank)
-forcefield=opls      	#### >>> Either opls* or gromos** at the moment
-gromoslipidonly=popc	#### >>> POPE, POPC and POPG all working
-z=10			#### >>> Simulation box z axis. 15 as standard
+pro=$1
+prot=${pro%.*}		#### >>> Needs to match input PDB
+nuc=ATP			#### >>> ADP or ATP (if none leave blank)
+forcefield=gromos      	#### >>> Either opls* or gromos** at the moment
+gromoslipidonly=pope	#### >>> POPE, POPC and POPG all working
 sol=spc			#### >>> spc, tip3p etc
 ters=no 		#### >>> Use for capping peptides
 propka=no		#### >>> do propka?
+z=14
 ############################
 
 # * OPLS-AA
@@ -37,14 +32,6 @@ if [[ "$1" == "-h" ]]; then echo "
 exit 0
 fi
 
-###### Change log 
-###### MODIFIED 24/11/2014 for new gmembed - including NUC in gmembed option###############
-###### MODIFIED 04/06/2015 for Gromos ff and new lipids ##################################
-###### ADDED OPLS on 16/06/2015 ############
-###### ADDED CHARMM 36 22/06/2015 ############
-###### Added peptide capping stuff 08/03/2016 #############
-###### Added propka stuff 28/02/2017 #############
-
 if [[ "$forcefield" == "gromos" ]]
         then lipid=$gromoslipidonly	## Can change if using ff with different lipids
 fi
@@ -54,22 +41,37 @@ fi
 #
 ## Shouldn't have to do anything after this bit
 #
+rm -f arg bottom chains his ndx pdboutput protonation asp chain1 glu lys ndx2 preprotonation_* prop1 512-popc-eq.pdb ${prot}_backup.gro  ${prot}.gro.pdb ${prot}-mem_backup.pdb.gro ${prot}-mem.pdb.gro ${prot}_postpdb2gmx.gro all_backup.gro all_backup.pdb all.gro all.pdb CLx.pdb conf.gro cons_pull.mdp e1.edr em1_backup.tpr em1.mdp em1.tpr em2.edr em2.tpr em_gmx5.mdp em.mdp em_short.mdp emshort.mdp em_sol.gro eq_fold1.mdp eq_fold2.mdp eq_npt.mdp forpropka_*.pdb forpropka_*.propka_input gmembed_backup.tpr gmembed.mdp gmembed_nuc.mdp gmembed_NUC.mdp gmembed_nuc_sol.mdp gmembed_pop.mdp gmembed.tpr index.ndx ions.gro ions_in.tpr ions_mod.gro md_fold.mdp md_GPU_gmx5_pull.mdp md_GPU_gmx5_soluble.mdp md_GPU.mdp md_GPU_POPE.mdp md.log md_posre.mdp md_sol.mdp membed.dat membedded_backup.gro memsol.ndx memsolpre.ndx Memx.gro Memx.pdb MGx.pdb NAx.pdb NPT.mdp NVT.mdp premembed_backup.gro premembed_backup.pdb premembed.gro premembed.pdb presol.gro presol_mod.gro pr_GPU_POPC_gmx5.mdp pr_GPU_POPC.mdp pr_GPU_POPC_test_gmx5.mdp pr_GPU_POPC_test.mdp protein.gro protein.pdb protmem.gro protmem.gro_backup.pdb protmem.gro.pdb pr_test.cpt pr_test.edr pr_test.gro pr_test.log pr_test.tpr ref1.pdb ref2.pdb solmem_backup.gro solmem.gro SOLx.gro SOLx.pdb state.cpt state_prev.cpt topol.top
 
+#
+## Define functions
+#
 
+ReportError () {
+if [ ! -f ${1} ]; then
+        echo "
+        #################################
+        Error: no ${1}
+        #################################"
+        exit 0
+else
+        echo "${1} done"
+fi
+}
 # get correct membrane and other setup files
 
 if [[ "$forcefield" == "opls" ]]
-	then cp /home/birac/Desktop/MD_KIT/popc_opls.itp . 
-	cp /home/birac/Desktop/MD_KIT/512-popc-eq.pdb .
-	cp /home/birac/Desktop/MD_KIT/*mdp .
-	cp /home/birac/Desktop/MD_KIT/membed.dat .
+	then cp $MM/popc_opls.itp . 
+	cp $MM/512-popc-eq.pdb .
+	cp $MM/MDPs/*mdp .
+	cp $MM/membed.dat .
 fi
 
 if [[ "$forcefield" == "gromos" ]]
-	then cp /home/birac/Desktop/Gromos53A6/MD_KIT/*itp . 
-	cp /home/birac/Desktop/Gromos53A6/MD_KIT/512-$lipid-gromos53a6.pdb .
-	cp /home/birac/Desktop/Gromos53A6/MD_KIT/*mdp .
-	cp /home/birac/Desktop/Gromos53A6/MD_KIT/membed.dat .
+	then cp $MM/ITPs/*itp . 
+	cp $MM/512-$lipid-gromos53a6.pdb .
+	cp $MM/MDPs/*mdp .
+	cp $MM/membed.dat .
 fi
 
 
@@ -98,7 +100,10 @@ mv *#* Overwrites
 ## 2;; Building simulation box and preparing for g_membed
 #
 
-editconf -f ${prot}.gro -o ${prot}.gro.pdb
+gmx_sse editconf -f ${prot}.gro -o ${prot}.gro.pdb
+
+ReportError ${prot}.gro.pdb
+
 sed -e 's/TER/   /g' -e 's/ENDMDL/      /g' ${prot}.gro.pdb -i
 
 mv protmem.gro.pdb protmem.gro_backup.pdb
@@ -124,22 +129,16 @@ mv ${prot}-mem.pdb.gro ${prot}-mem_backup.pdb.gro
 #
 
 if [ "$forcefield" == "gromos" ] && [ "$lipid" == "pope" ]
-        then editconf -f protmem.gro.pdb -o ${prot}-mem.pdb.gro -c -box 12.2528 12.3878 $z -bt triclinic
+        then gmx_sse editconf -f protmem.gro.pdb -o ${prot}-mem.pdb.gro -c -box 12.2528 12.3878 $z -bt triclinic
 elif [ "$forcefield" == "gromos" ] && [ "$lipid" == "popg" ]
-	then editconf -f protmem.gro.pdb -o ${prot}-mem.pdb.gro -c -box 13.2756 13.2546 $z -bt triclinic
+	then gmx_sse editconf -f protmem.gro.pdb -o ${prot}-mem.pdb.gro -c -box 13.2756 13.2546 $z -bt triclinic
 elif [ "$forcefield" == "gromos" ] && [ "$lipid" == "popc" ]
-        then editconf -f protmem.gro.pdb -o ${prot}-mem.pdb.gro -c -box 13.3356 13.3040 $z -bt triclinic
+        then gmx_sse editconf -f protmem.gro.pdb -o ${prot}-mem.pdb.gro -c -box 13.3356 13.3040 $z -bt triclinic
 elif [[ "$forcefield" == "opls" ]]
-	then editconf -f protmem.gro.pdb -o ${prot}-mem.pdb.gro -c -box 13.1708 13.1708 $z -bt triclinic
+	then gmx_sse editconf -f protmem.gro.pdb -o ${prot}-mem.pdb.gro -c -box 13.1708 13.1708 $z -bt triclinic
 fi
 
-if [ ! -f ${prot}-mem.pdb.gro ]; then
-        echo "
-        ###############################
-        Error in making simulation box!
-        ###############################"
-        exit 0
-fi
+ReportError ${prot}-mem.pdb.gro
 
 size=`tail -n 1 ${prot}-mem.pdb.gro`
 grep -v "^$" ${prot}-mem.pdb.gro > protmem.gro
@@ -149,18 +148,27 @@ grep -v "^$" ${prot}-mem.pdb.gro > protmem.gro
 #
 
 if [ "$forcefield" == "gromos" ] && [ "$lipid" == "pope" ]
-	then sed '/Include water topology/i \
+	then 
+	sed '/pope_GROMOS-CKP.itp/d' ${prot}.top -i
+	sed '/Include water topology/i \
 # include "'pope_GROMOS-CKP.itp'"\ ' ${prot}.top -i
 elif [ "$forcefield" == "gromos" ] && [ "$lipid" == "popg" ]
-        then sed '/Include water topology/i \
+        then 
+	sed '/dpopgGromacs4_53a6.itp/d' ${prot}.top -i
+	sed '/lpopgGromacs4_53a6.itp/d' ${prot}.top -i
+	sed '/Include water topology/i \
 # include "'dpopgGromacs4_53a6.itp'"\ ' ${prot}.top -i ; sed '/# include "'dpopgGromacs4_53a6.itp'"/i \
 # include "'lpopgGromacs4_53a6.itp'"\
 ' ${prot}.top -i
 elif [ "$forcefield" == "gromos" ] && [ "$lipid" == "popc" ]
-	then sed '/Include water topology/i \
+	then 
+	sed '/popcGromacs4_53a6.itp/d' ${prot}.top -i
+	sed '/Include water topology/i \
 # include "'popcGromacs4_53a6.itp'"\ ' ${prot}.top -i
 elif [[ "$forcefield" == "opls" ]]
-	then sed '/Include water topology/i \
+	then 
+	sed '/popc_opls.itp/d' ${prot}.top -i
+	sed '/Include water topology/i \
 # include "'popc_opls.itp'"\
 ' ${prot}.top -i
 fi
@@ -172,16 +180,10 @@ fi
 mv solmem.gro solmem_backup.gro
 
 if [[ "$forcefield" == "opls" ]]
-	then genbox -cp protmem.gro -o solmem.gro -p ${prot}.top -cs
+	then gmx_sse genbox -cp protmem.gro -o solmem.gro -p ${prot}.top -cs
 fi
 
-if [ "$forcefield" == "opls" ] && [ ! -f solmem.gro ]; then
-        echo "
-        ################################
-        Error in solvating opls membrane
-        ################################"
-        exit 0
-fi
+ReportError ${prot}-mem.pdb.gro
 
 mv premembed.gro premembed_backup.gro
 
@@ -204,15 +206,9 @@ if [[ ! "$forcefield" == "opls" ]]
         then cp protmem.gro premembed.gro
 fi
 
-editconf -f premembed.gro -o premembed.pdb
+gmx_sse editconf -f premembed.gro -o premembed.pdb
 
-if [ ! -f premembed.pdb ]; then
-        echo "
-        ################################
-        Error in solvating opls membrane
-        ################################"
-        exit 0
-fi
+ReportError premembed.pdb
 
 if [ "$forcefield" == "gromos" ] && [ "$lipid" == "popg" ]
 	then grep -e "DPO\|LPO" premembed.pdb> Memx.pdb
@@ -227,13 +223,13 @@ grep ${nuc} premembed.pdb> Nucx.pdb; fi
 grep MG premembed.pdb> MGx.pdb
 grep -e ACE -e NH2 -e NAC -e ALA -e CYS -e ASP -e GLU -e PHE -e GLY -e HIS -e ILE -e LYS -e LEU -e MET -e ASN -e PRO -e GLN -e ARG -e SER -e THR -e VAL -e TRP -e TYR premembed.pdb | grep ATOM > protein.pdb
 
-editconf -f protein.pdb -o protein.gro
-editconf -f Memx.pdb -o Memx.gro
-editconf -f SOLx.pdb -o SOLx.gro
-editconf -f NAx.pdb -o NAx.gro
-editconf -f CLx.pdb -o CLx.gro
-editconf -f Nucx.pdb -o Nucx.gro
-editconf -f MGx.pdb -o MGx.gro
+gmx_sse editconf -f protein.pdb -o protein.gro
+gmx_sse editconf -f Memx.pdb -o Memx.gro
+gmx_sse editconf -f SOLx.pdb -o SOLx.gro
+gmx_sse editconf -f NAx.pdb -o NAx.gro
+gmx_sse editconf -f CLx.pdb -o CLx.gro
+gmx_sse editconf -f Nucx.pdb -o Nucx.gro
+gmx_sse editconf -f MGx.pdb -o MGx.gro
 
 mv all.pdb all_backup.pdb
 
@@ -241,7 +237,7 @@ mv all.pdb all_backup.pdb
 
 cat MGx.pdb Nucx.pdb protein.pdb Memx.pdb SOLx.pdb NAx.pdb  CLx.pdb | grep -e MG -e ATP -e ADP -e ACE -e ALA -e CYS -e ASP -e GLU -e PHE -e GLY -e HIS -e ILE -e LYS -e LEU -e MET -e ASN -e PRO -e GLN -e ARG -e SER -e THR -e VAL -e TRP -e TYR -e NAC -e NH2 -e POP -e DPO -e LPO -e DDM -e SOL -e NA -e CL >> all.pdb
 
-editconf -f all.pdb -o all.gro
+gmx_sse editconf -f all.pdb -o all.gro
 
 if [[ ! -f all.gro ]]; then
 	echo "
@@ -313,7 +309,7 @@ rm index.ndx
 rm ndx
 rm ndx2
 
-echo q | make_ndx -f solmem.gro -o index > ndx
+echo q | gmx_sse make_ndx -f solmem.gro -o index > ndx
 
 cat ndx | grep [0-9] | grep atoms | awk '{print $2}' | sort | uniq -d > ndx2
 
@@ -335,13 +331,13 @@ memno=$(expr $groups + 0)
 nucno=$(expr $groups + 1)
 
 if [ "$forcefield" == "gromos" ] && [ "$lipid" == "popg" ]
-        then echo -e '"DPO"' '|' '"LPO"' '\n' '"MG"' '|' '"'$nuc'"' '|' '"Protein"' '\n' name $memno Mem '\n' name $nucno Nuc '\n'q  | make_ndx -f solmem.gro -o memsolpre.ndx -n index.ndx
+        then echo -e '"DPO"' '|' '"LPO"' '\n' '"MG"' '|' '"'$nuc'"' '|' '"Protein"' '\n' name $memno Mem '\n' name $nucno Nuc '\n'q  | gmx_sse make_ndx -f solmem.gro -o memsolpre.ndx -n index.ndx
 elif [ "$forcefield" == "gromos" ] && [ "$lipid" == "pope" ]
-        then echo -e '"POP"' '\n' '"MG"' '|' '"'$nuc'"' '|' '"Protein"' '\n' name $memno Mem '\n' name $nucno Nuc '\n'q  | make_ndx -f solmem.gro -o memsolpre.ndx -n index.ndx
+        then echo -e '"POP"' '\n' '"MG"' '|' '"'$nuc'"' '|' '"Protein"' '\n' name $memno Mem '\n' name $nucno Nuc '\n'q  | gmx_sse make_ndx -f solmem.gro -o memsolpre.ndx -n index.ndx
 elif [ "$forcefield" == "gromos" ] && [ "$lipid" == "popc" ]
-        then echo -e '"POP"' '\n' '"MG"' '|' '"'$nuc'"' '|' '"Protein"' '\n' name $memno Mem '\n' name $nucno Nuc '\n'q  | make_ndx -f solmem.gro -o memsolpre.ndx -n index.ndx
+        then echo -e '"POP"' '\n' '"MG"' '|' '"'$nuc'"' '|' '"Protein"' '\n' name $memno Mem '\n' name $nucno Nuc '\n'q  | gmx_sse make_ndx -f solmem.gro -o memsolpre.ndx -n index.ndx
 elif [[ "$forcefield" == "opls" ]]
-	then echo -e '"POP"' '\n' '"MG"' '|' '"'$nuc'"' '|' '"Protein"' '\n' name $memno Mem '\n' name $nucno Nuc '\n'q  | make_ndx -f solmem.gro -o memsolpre.ndx -n index.ndx
+	then echo -e '"POP"' '\n' '"MG"' '|' '"'$nuc'"' '|' '"Protein"' '\n' name $memno Mem '\n' name $nucno Nuc '\n'q  | gmx_sse make_ndx -f solmem.gro -o memsolpre.ndx -n index.ndx
 fi
 }
 
@@ -351,8 +347,9 @@ if [[ ! -z ${nuc} ]]; then
 	nucndx
 else 
 	memno=$(expr $groups + 0)
-	echo -e '"POP"' '\n' name $memno Mem'\n'q | make_ndx -f solmem.gro -o memsolpre.ndx -n index.ndx
+	echo -e '"POP"' '\n' name $memno Mem'\n'q | gmx_sse make_ndx -f solmem.gro -o memsolpre.ndx -n index.ndx
 fi
+
 
 ## Could consolidate some of the above loops but useful to keep them separated for future use
 
@@ -360,9 +357,9 @@ groups2=`grep -c '\[' memsolpre.ndx`
 memsolno=$(expr $groups2 + 0)
 
 if [ "$forcefield" == "gromos" ] && [ "$lipid" == "popg" ]
-	then echo -e '"Mem"' '|' '"Water_and_ions"' '\n'q | make_ndx -f solmem.gro -n memsolpre.ndx -o memsol.ndx
+	then echo -e '"Mem"' '|' '"Water_and_ions"' '\n'q | gmx_sse make_ndx -f solmem.gro -n memsolpre.ndx -o memsol.ndx
 	sed 's/Mem_Water_and_ions/Mem_SOL/' memsol.ndx -i
-else echo -e '"Mem"' '|' '"SOL"' '\n'q | make_ndx -f solmem.gro -n memsolpre.ndx -o memsol.ndx
+else echo -e '"Mem"' '|' '"SOL"' '\n'q | gmx_sse make_ndx -f solmem.gro -n memsolpre.ndx -o memsol.ndx
 fi
 #
 ## 7;; The DREADED g_membed stage - this is where most setups fail as membrane just explodes grrr.
@@ -389,11 +386,11 @@ fi
 ### Here the maxwarn is justifiable as sometimes the toplogy will have blank NA+CL lines
 
 if [[ ! -z "${nuc}" ]]; then
-grompp -v -f gmembed_nuc.mdp -o gmembed -c solmem.gro -p ${prot}.top -n memsol.ndx -maxwarn 4
+gmx_sse grompp -v -f gmembed_nuc.mdp -o gmembed -c solmem.gro -p ${prot}.top -n memsol.ndx -maxwarn 4
 fi
 
 if [[ -z "${nuc}" ]]; then
-grompp -v -f gmembed.mdp -o gmembed -c solmem.gro -p ${prot}.top -n memsol.ndx -maxwarn 4
+gmx_sse grompp -v -f gmembed.mdp -o gmembed -c solmem.gro -p ${prot}.top -n memsol.ndx -maxwarn 4
 fi
 
 if [ ! -f gmembed.tpr ]; then
@@ -407,9 +404,9 @@ fi
 mv membedded.gro membedded_backup.gro
 
 if [[ ! -z "${nuc}" ]]; then
-echo Nuc Mem | mdrun -s gmembed.tpr -membed membed.dat -o traj.trr -c membedded.gro -e ener.edr -cpt -1 -v -stepout 100 -mn memsol
+echo Nuc Mem | gmx_sse mdrun -s gmembed.tpr -membed membed.dat -o traj.trr -c membedded.gro -e ener.edr -cpt -1 -v -stepout 100 -mn memsol
 elif [[ -z "${nuc}" ]]; then
-echo Protein Mem | mdrun -s gmembed.tpr -membed membed.dat -o traj.trr -c membedded.gro -e ener.edr -cpt -1 -v -stepout 100 -mn memsol
+echo Protein Mem | gmx_sse mdrun -s gmembed.tpr -membed membed.dat -o traj.trr -c membedded.gro -e ener.edr -cpt -1 -v -stepout 100 -mn memsol
 fi
 
 if [ ! -f membedded.gro ]; then
@@ -471,7 +468,7 @@ echo CL `echo $totalatomssol $uniqatomssol | awk '{print $1/$2}'` >> ${prot}.top
 
 mv em1.tpr em1_backup.tpr
 
-grompp -v -f em.mdp -o em1 -e em1 -c membedded.gro  -p ${prot}.top -maxwarn 3
+gmx_sse grompp -v -f em.mdp -o em1 -c membedded.gro  -p ${prot}.top -maxwarn 3
 
 if [ ! -f em1.tpr ]; then
         echo "
@@ -483,9 +480,9 @@ fi
 
 mv after_em1.gro after_em1_backup.gro
 
-mdrun -v -s em1 -o em1 -e e1 -c after_em1.gro 
+gmx_sse mdrun -v -s em1 -o em1 -c after_em1.gro 
 
-editconf -f after_em1.gro -o after_em1.pdb
+gmx_sse editconf -f after_em1.gro -o after_em1.pdb
 
 if [ ! -f after_em1.gro ]; then
         echo "
@@ -522,11 +519,11 @@ if [ ! -f em_sol.gro ]; then
         exit 0
 fi
 
-editconf -f em1.tpr -o ref1.pdb
+gmx_sse editconf -f em1.tpr -o ref1.pdb
 
 mv ions_in.tpr ions_in_backup.tpr
 
-grompp -f em.mdp -c em_sol.gro -p ${prot}.top -o ions_in
+gmx_sse grompp -f em.mdp -c em_sol.gro -p ${prot}.top -o ions_in
 
 if [ ! -f ions_in.tpr ]; then
         echo "
@@ -568,7 +565,7 @@ echo CL `echo $totalatomssol $uniqatomssol | awk '{print $1/$2}'` >> ${prot}.top
 
 mv em2.tpr em2_backup.tpr
 
-grompp -v -f em -o em2 -c ions_mod.gro -p ${prot}.top
+gmx_sse grompp -v -f em -o em2 -c ions_mod.gro -p ${prot}.top
 
 if [ ! -f em2.tpr ]; then
         echo "
@@ -578,26 +575,26 @@ if [ ! -f em2.tpr ]; then
         exit 0
 fi
 
-mdrun -v -s em2 -o em2 -e em2 -c after_em2
+gmx_sse mdrun -v -s em2 -o em2  -c after_em2
 
-editconf -f after_em2.gro -o after_em2.pdb
+gmx_sse editconf -f after_em2.gro -o after_em2.pdb
 
-editconf -f em2.tpr -o ref2.pdb
+gmx_sse editconf -f em2.tpr -o ref2.pdb
 
 #
 ## 10;; Prepare for equilibration
 #
 
-#grompp -f pr_POPC_berger_test.mdp -c after_em2.gro -p ${prot}.top -o pr_test.tpr
-#grompp -f pr_POPC_berger.mdp -c after_em2.gro -p ${prot}.top -o pr_full.tpr
+#gmx_sse grompp -f pr_POPC_berger_test.mdp -c after_em2.gro -p ${prot}.top -o pr_test.tpr
+#gmx_sse grompp -f pr_POPC_berger.mdp -c after_em2.gro -p ${prot}.top -o pr_full.tpr
 # pr_POPC_berger.mdp is fucked! No pressure!
 
-grompp -f pr_GPU_POPC_test.mdp -c after_em2.gro -p ${prot}.top -o pr_test.tpr
-grompp -f pr_GPU_POPC.mdp -c after_em2.gro -p ${prot}.top -o pr_full.tpr
+gmx_sse grompp -f pr_GPU_POPC_test.mdp -c after_em2.gro -p ${prot}.top -o pr_test.tpr
+gmx_sse grompp -f pr_GPU_POPC.mdp -c after_em2.gro -p ${prot}.top -o pr_full.tpr
 
 mv pr_test.gro pr_test_backup.gro
 
-mdrun -v -deffnm pr_test
+gmx_sse mdrun -v -deffnm pr_test
 
 if [ ! -f pr_test.gro ]; then
         echo "
@@ -609,7 +606,7 @@ elif [ -f pr_test.gro ]; then
         echo "
         ##########################################
         Equilibration seems fine. Run a 1 ns equil
-        with: mdrun -v -deffnm pr_full >& out &
+        with: gmx_sse mdrun -v -deffnm pr_full >& out &
         ##########################################"
         exit 0
 fi
